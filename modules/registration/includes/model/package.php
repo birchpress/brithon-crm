2f83aa18f8d5ef2e24786649270e1a561d218de7
Wei Su <wsu@birchpress.com>
2015-08-20 21:37:31 +0800

    Modify php files to meet Wordpress code standard

--- modules/registration/includes/model/package.php
+++ modules/registration/includes/model/package.php
@@ -2,99 +2,99 @@
 
 birch_ns( 'brithoncrm.registration', function( $ns ) {
 
-        global $brithoncrm;
+		global $brithoncrm;
 
-        $ns->register_account = function() use ( $ns ) {
+		$ns->register_account = function() use ( $ns ) {
 
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-            $email = $_POST['email'];
-            $first_name = $_POST['first_name'];
-            $last_name = $_POST['last_name'];
-            $org = $_POST['org'];
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			$email = $_POST['email'];
+			$first_name = $_POST['first_name'];
+			$last_name = $_POST['last_name'];
+			$org = $_POST['org'];
 
-            if ( ! $username ) {
-                $ns->return_err_msg( 'Empty username!' );
-            }
-            if ( ! $password ) {
-                $ns->return_err_msg( 'Empty password!' );
-            }
-            if ( ! $email ) {
-                $ns->return_err_msg( 'Empty email address!' );
-            }
-            if ( ! $first_name ) {
-                $ns->return_err_msg( 'First name required!' );
-            }
-            if ( ! $last_name ) {
-                $ns->return_err_msg( 'Last name required!' );
-            }
-            if ( ! $org ) {
-                $ns->return_err_msg( 'Organization required!' );
-            }
+			if ( ! $username ) {
+				$ns->return_err_msg( 'Empty username!' );
+			}
+			if ( ! $password ) {
+				$ns->return_err_msg( 'Empty password!' );
+			}
+			if ( ! $email ) {
+				$ns->return_err_msg( 'Empty email address!' );
+			}
+			if ( ! $first_name ) {
+				$ns->return_err_msg( 'First name required!' );
+			}
+			if ( ! $last_name ) {
+				$ns->return_err_msg( 'Last name required!' );
+			}
+			if ( ! $org ) {
+				$ns->return_err_msg( 'Organization required!' );
+			}
 
-            $userdata = array(
-                'user_login' => $username,
-                'user_pass' => $password,
-                'user_email' => $email,
-                'display_name' => "$first_name $last_name",
-                'nickname' => "$first_name $last_name",
-                'first_name' => $first_name,
-                'last_name' => $last_name,
-            );
+			$userdata = array(
+				'user_login' => $username,
+				'user_pass' => $password,
+				'user_email' => $email,
+				'display_name' => "$first_name $last_name",
+				'nickname' => "$first_name $last_name",
+				'first_name' => $first_name,
+				'last_name' => $last_name,
+			);
 
-            $user_id = wp_insert_user( $userdata );
-            $subdir = $ns->generate_blog_dir( $first_name, $last_name );
+			$user_id = wp_insert_user( $userdata );
+			$subdir = $ns->generate_blog_dir( $first_name, $last_name );
 
-            if ( ! is_wp_error( $user_id ) ) {
-                add_user_meta( $user_id, 'organization', $org );
-                $site_id = wpmu_create_blog( $ns->get_clean_basedomain(),
-                    $subdir, "$first_name $last_name", $user_id );
+			if ( ! is_wp_error( $user_id ) ) {
+				add_user_meta( $user_id, 'organization', $org );
+				$site_id = wpmu_create_blog( $ns->get_clean_basedomain(),
+					$subdir, "$first_name $last_name", $user_id );
 
-                if ( ! is_wp_error( $site_id ) ) {
-                    $creds = array();
-                    $creds['user_login'] = $username;
-                    $creds['user_password'] = $password;
-                    $creds['remember'] = true;
-                    $usr = wp_signon( $creds, false );
+				if ( ! is_wp_error( $site_id ) ) {
+					$creds = array();
+					$creds['user_login'] = $username;
+					$creds['user_password'] = $password;
+					$creds['remember'] = true;
+					$usr = wp_signon( $creds, false );
 
-                    die( json_encode(
-                            array(
-                                'user_id' => $user_id,
-                                'site_id' => $site_id,
-                                'site_dir' => $subdir,
-                            ) ) );
-                } else {
-                    $ns->return_err_msg( $site_id->get_error_message( $site_id->get_error_code() ) );
-                }
-            } else {
-                $ns->return_err_msg( $user_id->get_error_message( $user_id->get_error_code() ) );
-            }
-        };
+					die( json_encode(
+						array(
+								'user_id' => $user_id,
+								'site_id' => $site_id,
+								'site_dir' => $subdir,
+							) ) );
+				} else {
+					$ns->return_err_msg( $site_id->get_error_message( $site_id->get_error_code() ) );
+				}
+			} else {
+				$ns->return_err_msg( $user_id->get_error_message( $user_id->get_error_code() ) );
+			}
+		};
 
-        $ns->return_err_msg = function( $msg, $error = 'Error' ) use ( $ns ) {
-            die( json_encode( array(
-                        'error' => $error,
-                        'message' => $msg,
-                    ) ) );
-        };
+		$ns->return_err_msg = function( $msg, $error = 'Error' ) use ( $ns ) {
+			die( json_encode( array(
+						'error' => $error,
+						'message' => $msg,
+					) ) );
+		};
 
-        $ns->return_result = function( $succeed, $data ) use ( $ns ) {
-            return array(
-                'succeed' => $succeed,
-                'data' => $data,
-            );
-        };
+		$ns->return_result = function( $succeed, $data ) use ( $ns ) {
+			return array(
+				'succeed' => $succeed,
+				'data' => $data,
+			);
+		};
 
-        $ns->get_clean_basedomain = function() use ( $ns ) {
-            $domain = preg_replace( '|https?://|', '', site_url() );
-            if ( $slash = strpos( $domain, '/' ) ) {
-                $domain = substr( $domain, 0, $slash );
-            }
-            return $domain;
-        };
+		$ns->get_clean_basedomain = function() use ( $ns ) {
+			$domain = preg_replace( '|https?://|', '', site_url() );
+			if ( $slash = strpos( $domain, '/' ) ) {
+				$domain = substr( $domain, 0, $slash );
+			}
+			return $domain;
+		};
 
-        $ns->generate_blog_dir = function( $first_name, $last_name ) use ( $ns ) {
-            return '/'.$first_name.'_'.$last_name.rand();
-        };
+		$ns->generate_blog_dir = function( $first_name, $last_name ) use ( $ns ) {
+			return '/'.$first_name.'_'.$last_name.rand();
+		};
 
-    } );
+} );
--- modules/subscriptions/includes/view/package.php
+++ modules/subscriptions/includes/view/package.php
@@ -2,25 +2,25 @@
 
 birch_ns( 'brithoncrm.subscriptions', function( $ns ) {
 
-        global $brithoncrm;
+		global $brithoncrm;
 
-        $ns->plugin_init = function() use ( $ns ) {
-            register_post_type( 'subscription' );
-        };
+		$ns->plugin_init = function() use ( $ns ) {
+			register_post_type( 'subscription' );
+		};
 
-        $ns->wp_admin_init = function() use ( $ns, $brithoncrm ) {
+		$ns->wp_admin_init = function() use ( $ns, $brithoncrm ) {
 
-        };
+		};
 
-        $ns->create_admin_menus = function() use ( $ns ) {
-            add_menu_page( 'Billing and invoices', 'Settings', 'read',
-                'brithoncrm/subscriptions', array( $ns, 'render_setting_page' ), '', 81 );
-        };
-        $ns->render_setting_page = function() use ( $ns ) {
+		$ns->create_admin_menus = function() use ( $ns ) {
+			add_menu_page( 'Billing and invoices', 'Settings', 'read',
+				'brithoncrm/subscriptions', array( $ns, 'render_setting_page' ), '', 81 );
+		};
+		$ns->render_setting_page = function() use ( $ns ) {
 ?>
-            <h3>Billing and invoices</h3>
-            <section id="birchpress-settings"></section>
+			<h3>Billing and invoices</h3>
+			<section id="birchpress-settings"></section>
 <?php
-        };
+		};
 
-    } );
+} );
