af792e160c0694c431d76e93c17acdeaca3d91c2
Wei Su <wsu@birchpress.com>
2015-09-01 18:31:59 +0800

    Work out global search function

--- modules/common/model/datatables/package.php
+++ modules/common/model/datatables/package.php
@@ -32,9 +32,29 @@ birch_ns( 'brithoncrm.common.datatables.model', function( $ns ) {
             $data_obj = json_decode( $data );
             $data_arr = $data_obj->data;
             $total = count( $data_arr );
+            $total_filtered = $total;
 
             if ( $start < $total ) {
                 $result = new stdClass();
+
+                // Global search
+                $search = $_POST['search'];
+                $columns_info = $_POST['columns'];
+                if ( $search['value'] ) {
+                    $searchable_cols = array();
+                    $search_result = array();
+                    foreach ( $columns_info as $id => $val ) {
+                        if ( $val['searchable'] === 'true' ) {
+                            array_push( $searchable_cols, $id );
+                        }
+                    }
+                    foreach ( $searchable_cols as $id ) {
+                        $search_result = array_merge( $search_result, $ns->search( $data_arr, $search['value'], $id ) );
+                    }
+                    $data_arr = $search_result;
+                    $total_filtered = count( $data_arr );
+                }
+
                 $part = array_slice( $data_arr, $start, $length );
 
                 foreach ( $_POST['order'] as $order ) {
@@ -53,7 +73,7 @@ birch_ns( 'brithoncrm.common.datatables.model', function( $ns ) {
                 $result->data = $part;
                 $result->draw = $draw;
                 $result->recordsTotal = $total;
-                $result->recordsFiltered = $total;
+                $result->recordsFiltered = $total_filtered;
                 die( json_encode( $result ) );
             } else {
                 die( '{"data": {}}' );
@@ -79,17 +99,17 @@ birch_ns( 'brithoncrm.common.datatables.model', function( $ns ) {
 
         $ns->column_comparater = function( $element1, $element2, $id, $asc = true ) use ( $ns ) {
             // Try converting number first
-            if ( preg_match('/^[0-9]+$/', $element1[$id]) && preg_match('/^[0-9]+$/', $element2[$id])) {
-                $num1 = intval($element1[$id]);
-                $num2 = intval($element2[$id]);
-                return $asc ? ($num1 - $num2) : ($num2 - $num1);
+            if ( preg_match( '/^[0-9]+$/', $element1[$id] ) && preg_match( '/^[0-9]+$/', $element2[$id] ) ) {
+                $num1 = intval( $element1[$id] );
+                $num2 = intval( $element2[$id] );
+                return $asc ? ( $num1 - $num2 ) : ( $num2 - $num1 );
             } else {
                 $currency = 'USD';
                 $format = numfmt_create( 'en_US', NumberFormatter::CURRENCY );
-                $num1 = numfmt_parse_currency($format, $element1[$id], $currency);
-                $num2 = numfmt_parse_currency($format, $element2[$id], $currency);
-                if ($num1 && $num2) {
-                    return $asc ? ($num1 - $num2) : ($num2 - $num1);
+                $num1 = numfmt_parse_currency( $format, $element1[$id], $currency );
+                $num2 = numfmt_parse_currency( $format, $element2[$id], $currency );
+                if ( $num1 && $num2 ) {
+                    return $asc ? ( $num1 - $num2 ) : ( $num2 - $num1 );
                 }
             }
             if ( $asc ) {
@@ -97,6 +117,17 @@ birch_ns( 'brithoncrm.common.datatables.model', function( $ns ) {
             } else {
                 return strcmp( $element2[$id], $element1[$id] );
             }
+        };
+
+        $ns->search = function( $data, $pattern, $col_id ) use ( $ns ) {
+            // This is a very simple searching method. Just for demo
+            $result = array();
+            foreach ( $data as $item ) {
+                if ( stripos( $item[$col_id], $pattern ) !== false ) {
+                    array_push( $result, $item );
+                }
+            }
+            return $result;
         };
 
         $ns->return_err_msg = function( $msg, $error = 'Error' ) use ( $ns ) {
