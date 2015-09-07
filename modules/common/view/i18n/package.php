68fa6fbe2d31cecfbd657e032fc3310e23e9d48c
Wei Su <wsu@birchpress.com>
2015-09-07 16:33:35 +0800

    Move language directory to the root of the plugin

--- modules/common/view/i18n/package.php
+++ modules/common/view/i18n/package.php
@@ -2,23 +2,23 @@
 
 birch_ns( 'brithoncrm.common.view.i18n', function( $ns ) {
 
-        global $brithoncrm;
+		global $brithoncrm;
 
-        $ns->init = function() use ( $ns ) {
-            add_action( 'plugins_loaded', array( $ns, 'load_i18n' ) );
-        };
+		$ns->init = function() use ( $ns ) {
+			add_action( 'plugins_loaded', array( $ns, 'load_i18n' ) );
+		};
 
-        $ns->load_i18n = function() use ( $ns, $brithoncrm ) {
-            $lang_dir = 'brithon-crm/modules/common/languages';
-            $res = load_plugin_textdomain( 'brithoncrm', false, $lang_dir );
-        };
+		$ns->load_i18n = function() use ( $ns, $brithoncrm ) {
+			$lang_dir = 'brithon-crm/languages';
+			$res = load_plugin_textdomain( 'brithoncrm', false, $lang_dir );
+		};
 
-        $ns->get_po_file = function() use ( $ns, $brithoncrm ) {
-            $path = $brithoncrm->plugin_url() . '/modules/common/languages';
-            $text_domain = 'brithoncrm';
-            $locale = get_locale();
-            $filename = "$path/$text_domain-$locale.po";
-            
-            return file_get_contents( $filename );
-        };
-    } );
+		$ns->get_po_file = function() use ( $ns, $brithoncrm ) {
+			$path = $brithoncrm->plugin_url() . '/languages';
+			$text_domain = 'brithoncrm';
+			$locale = get_locale();
+			$filename = "$path/$text_domain-$locale.po";
+
+			return file_get_contents( $filename );
+		};
+} );
