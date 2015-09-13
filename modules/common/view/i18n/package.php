c1b6d1e4335e27375a6bdf69232b15d63bdd2ca6
Wei Su <wsu@birchpress.com>
2015-09-13 17:47:55 +0800

    Dynamically calculate the directory where language files are located

--- modules/common/view/i18n/package.php
+++ modules/common/view/i18n/package.php
@@ -8,8 +8,15 @@ birch_ns( 'brithoncrm.common.view.i18n', function( $ns ) {
 			add_action( 'plugins_loaded', array( $ns, 'load_i18n' ) );
 		};
 
+		$ns->get_lang_path = function( $subdir = '/languages' ) use ( $ns ) {
+			$basename = plugin_basename( __FILE__ );
+			$arr = explode( '/', $basename );
+			$path = $arr[0] . $subdir;
+			return $path;
+		};
+
 		$ns->load_i18n = function() use ( $ns, $brithoncrm ) {
-			$lang_dir = 'brithon-crm/languages';
+			$lang_dir = $ns->get_lang_path();
 			load_plugin_textdomain( 'brithoncrm', false, $lang_dir );
 		};
 
