e651ed9dbef15255dddafd35d760f9ded60431b3
helloqiu <695441493@qq.com>
2015-09-15 20:41:43 +0800

    add a simple test of subscription

---
+++ tests/test-subscriptions.php
@@ -0,0 +1,17 @@
+<?php
+
+class SampleTest extends WP_UnitTestCase {
+	function test_get_max_provider() {
+		global $brithoncrm;
+		$returnId = $brithoncrm->subscriptions->model->get_max_providers( 1 );
+		$this->assertEquals( 1, $returnId );
+		$returnId = $brithoncrm->subscriptions->model->get_max_providers( 2 );
+		$this -> assertEquals( 5, $returnId );
+		$returnId = $brithoncrm->subscriptions->model->get_max_providers( 3 );
+		$this -> assertEquals( 10, $returnId );
+		$returnId = $brithoncrm->subscriptions->model->get_max_providers( 4 );
+		$this -> assertEquals( 20, $returnId );
+		$returnId = $brithoncrm->subscriptions->model->get_max_providers( 5 );
+		$this -> assertEquals( 0, $returnId );
+	}
+}
