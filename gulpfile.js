e8723376c8dfce2090e053aa0fc101016fa25089
Ray Xue <ray@brithon.com>
2015-09-19 00:23:47 +0800

    Allow product-customized file list.

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,56 @@ var gulp = require('gulp');
 
 var builder = require('birchpress-builder')(gulp);
 
-/* You can override the task in builder.
+/*
+ Files will not be included. Some files will be processed independently, such
+ as `birchpress`, `assets`, `modules` and `lib`. The variable
+ `coreMainSrcExclusion` is used to list those file to be excluded. We'd like you
+ to list them all for each product, even some of them, such as `package.json`
+ are common in different products.
+ */
+builder.taskConfig.coreMainSrcExclusion = [
+  // internal dev files
+  '!package.json',
+  '!gulpfile.js',
+  '!README.md',
+  '!phpunit.xml',
+  '!node_modules{,/**}',
+  '!test{,/**}',
+  '!__tests__{,/**}',
+  '!buildfiles{,/**}',
+  '!dist{,/**}',
 
-gulp.task('default', function() {
-  console.log('brithon crm');
-});
+  // the framework
+  '!birchpress{,/**}',
 
-*/
+  // independent filter rules
+  '!modules{,/**}',
+  '!lib{,/**}',
 
-/* You can also create new tasks.
+  // publish to wordpress
+  '!screenshots{,/**}',
+  '!readme.txt'
+];
 
-gulp.task('hello', function() {
-  console.log('world!');
-});
+/*
+ For wordpress plugins, `screenshots` and `readme.txt` are required to get
+ published. However, they will not be included in `free` version.
+ */
+builder.taskConfig.corePublishSrc = ['screenshots/**/*', 'readme.txt'];
 
-*/
+/*
+ You can override the task in builder.
+ */
+
+// gulp.task('default', function() {
+//   console.log('brithon crm');
+// });
+
+
+/*
+ You can also create new tasks.
+ */
+
+// gulp.task('hello', function() {
+//   console.log('world!');
+// });
