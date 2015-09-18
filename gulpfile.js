0bb2ce84d3c9c00fb28c7943f2fe6ef856372c40
Ray Xue <ray@brithon.com>
2015-09-18 22:27:56 +0800

    Add gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var builder = require('birchpress-builder')(gulp);
+
+/* You can override the task in builder.
+
+gulp.task('default', function() {
+  console.log('brithon crm');
+});
+
+*/
+
+/* You can also create new tasks.
+
+gulp.task('hello', function() {
+  console.log('world!');
+});
+
+*/
