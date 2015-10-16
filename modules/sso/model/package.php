d32e8462a3526ad69e2be563ce887a03de725eac
Wei Su <wsu@birchpress.com>
2015-10-16 19:08:51 +0800

    Token functions && implement db access funcs in framework

--- modules/sso/model/package.php
+++ modules/sso/model/package.php
@@ -1,5 +1,6 @@
 <?php
 
+
 birch_ns( 'brithoncrm.sso.model', function( $ns ) {
 
 		global $brithoncrm;
@@ -14,6 +15,7 @@ birch_ns( 'brithoncrm.sso.model', function( $ns ) {
 
 			$datatype = 'product';
 			register_post_type( $datatype );
+			register_post_type( 'token' );
 		};
 
 		$ns->wp_init = function() use ( $ns, $brithoncrm ) {
@@ -24,6 +26,58 @@ birch_ns( 'brithoncrm.sso.model', function( $ns ) {
 				add_action( 'wp_ajax_nopriv_brithoncrm_register', array( $ns, 'user_register' ) );
 				add_action( 'wp_ajax_brithoncrm_get_user_info', array( $ns, 'get_user_info' ) );
 			}
+		};
+
+		$ns->create_token = function() use ( $ns ) {
+			global $birchpress;
+
+			$rand_str = md5(rand(1, 100000) . time() . rand(1, 10000));
+
+			$birchpress->model->save(
+				array(
+					'post_type' => 'token',
+					'post_title' => $rand_str,
+					'_time' => time()
+				),
+				array(
+					'post_title', '_time'
+				)
+			);
+
+			return $rand_str;
+		};
+
+		$ns->check_token = function( $token ) use ( $ns ) {
+			global $birchpress;
+			$expiration_seconds = 600;
+
+			$query = $birchpress->model->query(
+				array(
+					'post_type' => 'product'
+				),
+				array(
+					'post_title', '_time'
+				)
+			);
+
+			for( $query as $id => $item ) {
+				$answer = $item['post_title'];
+				$creation_time = intval( $item['_time'] );
+				$now = time();
+
+				if($answer === $token) {
+					// Disallow expired token
+					if( $now > $creation_time + $expiration_seconds ) {
+						return false;
+					} else {
+						return true;
+					}
+					// Destroy the token after validation
+					$birchpress->db->delete( $id );
+				}
+			}
+
+			return false;
 		};
 
 		$ns->user_login = function() use ( $ns ) {
@@ -59,12 +113,15 @@ birch_ns( 'brithoncrm.sso.model', function( $ns ) {
 		$ns->add_product = function( $name, $domain = 'brithon.com' ) use ( $ns, $brithoncrm ) {
 			global $birchpress;
 
-			$birchpress->model->save(
+			return $birchpress->model->save(
 				array(
 					'post_type' => 'product',
 					'post_title' => $name,
 					'_product_name' => $name,
 					'_product_site' => "$name.$domain"
+				),
+				array(
+					'post_title', '_product_name', '_product_site'
 				)
 			);
 		};
@@ -77,15 +134,46 @@ birch_ns( 'brithoncrm.sso.model', function( $ns ) {
 			$query = $birchpress->model->query(
 				array(
 					'post_type' => 'product'
+				),
+				array(
+					'post_title', '_product_name', '_product_site'
 				)
 			);
-			while ( $query->have_posts() ) {
-				$meta = get_post_meta( the_id() );
-				array_push( $result, $meta );
+			
+			for( $query as $id => $item ) {
+				array_push( $result, array(
+					'id' => $id,
+					'name' => $item['post_title'],
+					'site' => $item['_product_site']
+				) );
 			}
 
 			return $result;
 		};
+
+		$ns->edit_product = function( $id, $name, $domain = 'brithon.com' ) use ( $ns ) {
+			global $birchpress;
+
+			$query = $birchpress->model->query(
+				array(
+					'post_type' => 'product'
+				),
+				array(
+					'post_title', '_product_name', '_product_site'
+				)
+			);
+
+			$query[$id]['post_title'] = $name;
+			$query[$id]['_product_name'] = $name;
+			$query[$id]['_product_site'] = "$name.$domain'";
+
+			return $birchpress->model->save($query[$id]);
+		};
+
+		$ns->delete_product = function( $id ) use ( $ns ) {
+			// In fact will be moved to trash
+			return wp_delete_post( $id, false );
+		}
 
 		$ns->request = function($url, $method, $data) use ( $ns ) {
 			$context = array(
@@ -97,5 +185,5 @@ birch_ns( 'brithoncrm.sso.model', function( $ns ) {
 			);
 			$context = stream_context_create($context);
 			return file_get_contents($url, false, $context);
-		}
+		};
 	} );
