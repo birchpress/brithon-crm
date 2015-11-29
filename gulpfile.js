de873e267e3cc123d6f052d93538ff80204f720d
Ray Xue <ray@brithon.com>
2015-11-29 22:13:32 +0800

    Support option --sroucemap.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const es = require('event-stream');
-const globby = require('globby');
 const browserify = require('browserify');
 const logger = require('gulp-logger');
-const source = require('vinyl-source-stream');
+const lazypipe = require('lazypipe');
+const through = require('through2');
 const rename = require('gulp-rename');
-const buffer = require('vinyl-buffer');
 const sourcemaps = require('gulp-sourcemaps');
 const gulp = require('gulp');
 
@@ -15,46 +13,101 @@ const builder = require('birchpress-builder')(gulp);
 const productConfig = {
 
   coreMainSrc: [
-   'brithon-crm.php',
-   'index.php',
-   'package.php',
-   'loader.php',
+    'brithon-crm.php',
+    'index.php',
+    'package.php',
+    'loader.php',
 
-   'languages/**/*'
+    'languages/**/*'
   ]
 
 };
 
 builder.setProductConfig(productConfig);
 
-const bundleFiles = ['modules/**/assets/js/apps/**/index.js'];
+const browserifyEntries = ['modules/**/assets/js/apps/**/index.js'];
+
+const browserifyTransform = [
+  [
+    'babelify',
+    {
+      'presets': [
+        'react'
+      ]
+    }
+  ],
+  [
+    'browserify-shim',
+    {
+      global: true
+    }
+  ],
+  'pkgify'
+];
+
+
+const browserifyOpts = {
+  transform: browserifyTransform
+};
+
+const debugBrowserifyOpts = {
+  debug: true,
+  transform: browserifyTransform
+};
+
+function bundler(debug) {
+  const opts = debug ? debugBrowserifyOpts : browserifyOpts;
+
+  return through.obj(function(file, enc, next) {
+    return browserify(file.path, opts)
+      .bundle(function(err, res) {
+        file.contents = res;
+        next(null, file);
+      });
+  });
+}
+
+function renamePipe(extName) {
+  const extname = (typeof extName !== 'undefined') ? extName : '.bundle.js';
+  const renameChannel = lazypipe()
+    .pipe(logger, {
+      extname,
+      showChange: true
+    })
+    .pipe(rename, {
+      extname
+    });
+
+  return renameChannel();
+}
+
+function sourceMapPipe(debug, dest) {
+  const smChannel = lazypipe()
+    .pipe(sourcemaps.init, {
+      loadMaps: true
+    })
+    .pipe(logger, {
+      before: 'extracting sourcemaps...',
+      after: 'sourcemaps extracted.',
+      extname: '.map',
+      showChange: true
+    })
+    .pipe(sourcemaps.write, dest);
+
+  const wrapperChannel = lazypipe()
+    .pipe(() => builder.gPlugins.if(debug, smChannel()));
+
+  return wrapperChannel();
+}
+
+const needSourceMap = builder.gPlugins.util.env.hasOwnProperty('sourcemap');
 
 gulp.task('bundle', function() {
-  const tasks = globby.sync(bundleFiles).map(indexjs => {
-    return browserify(indexjs, {
-        debug: true
-      })
-      .transform('babelify', {
-        presets: ['react']
-      })
-      .transform('browserify-shim', {
-        global: true
-      })
-      .transform('pkgify')
-      .bundle()
-      .pipe(source(indexjs))
-      .pipe(logger({
-        extname: '.bundle.js',
-        showChange: true
-      }))
-      .pipe(rename({
-        extname: '.bundle.js'
-      }))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({loadMaps: true}))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./'));
-  });
-
-  return es.merge(...tasks);
+  return gulp.src(browserifyEntries, {
+    cwdbase: true
+  })
+    .pipe(bundler(needSourceMap))
+    .pipe(renamePipe())
+    .pipe(sourceMapPipe(needSourceMap, './'))
+    .pipe(gulp.dest('./'));
 });
